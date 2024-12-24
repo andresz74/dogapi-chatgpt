d06fde982c16a1f684685f67274401816bca8776
Andres Zenteno <azenteno74@gmail.com>
2024-12-24 00:51:10 -0500

    feat: update components

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,1 +1,2 @@
-export const API_URL = 'http://localhost:3001';
+export const DOG_API_URL = 'https://dog-express-project.vercel.app'; //'http://localhost:3001';
+export const CHATGPT_API_URL = 'https://chat-gpt-access.vercel.app'; //'http://localhost:3001';
